ec4d7316ae1566fbdf6981066ece1d0f94efcf16
Noguchi Norimasa <norimasanoguchi@gmail.com>
2019-05-03 06:45:34 +0000

    #67アジェンダ削除機能および削除通知メール実装

--- app/mailers/transfer_authority_mailer.rb
+++ app/mailers/transfer_authority_mailer.rb
@@ -1,6 +1,6 @@
 class TransferAuthorityMailer < ApplicationMailer
 
-def transfer_done_mail(email,team_name)
+ def transfer_done_mail(email,team_name)
     @email = email
     @team_name = team_name
     mail to: @email, subject: 'リーダー権限が移譲されました'
